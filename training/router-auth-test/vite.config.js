cf9190cfb18f71c6a1d098954a5f6cd14ca68bae
Gi-in Jeong <giinie72@gmail.com>
2024-03-25 21:23:59 +0900

    Vue.js 애플리케이션에 대한 사용자 인증 구현
    홈, 사용자, 관리자 및 로그인을 포함한 여러 Vue.js 애플리케이션 페이지에 새로운 사용자 인증 기능이 추가되었습니다. 보안 액세스를 보장하기 위해 AuthUtil.js 유틸리티 및 라우터 가드도 통합되어 사용자 역할을 확인합니다. 코드베이스에는 API에서 코드 가져오기, 동적 탐색 조건 및 라우터 인덱스의 향상된 오류 처리 기능이 포함되어 있습니다. 사용자 경험을 향상시키기 위해 추가 구성 요소와 페이지가 추가되거나 업데이트되었습니다. 이에 따라 종속성 패키지도 업데이트되었습니다.

--- training/router-auth-test/vite.config.js
+++ training/router-auth-test/vite.config.js
@@ -2,14 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { manualChunksPlugin } from 'vite-plugin-webpackchunkname'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), manualChunksPlugin() ],
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
+  }
 })
--- training/router-test/src/pages/Home.vue
+++ training/router-test/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="card card-body">
-        <h2>Home</h2>
-    </div>
+  <div class="card card-body">
+      <h2>Home</h2>
+  </div>
 </template>
 
 <script>
